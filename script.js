e9a38c90b788c8c997b2b136ac593e2d12bd1dee
Kevin Castillo <kevinacastles@gmail.com>
2017-09-25 03:44:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	
 	$("#tour").hide();
 	$("#entretenimiento").hide();
-	
-     $("#s1").click(function(){
-       $("#tour").hide();
-	$("#entretenimiento").hide();
-			 $("#vina").show();
-			 
-    });
-    $("#s2").click(function(){
-        $("#tour").show();
-	$("#entretenimiento").hide();
-			 $("#vina").hide();
-});
+
+	$("#s1").click(function() {
+		$("#tour").hide();
+		$("#entretenimiento").hide();
+		$("#vina").show();
+	});
+	$("#s2").click(function() {
+		$("#tour").show();
+		$("#entretenimiento").hide();
+		$("#vina").hide();
+	});
 });
