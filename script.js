fec87a9fffd2a8ec6a09ef1c050d5419a028a820
Kevin Castillo <kevinacastles@gmail.com>
2017-10-31 19:48:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,255 +1,107 @@
-html,body
-{
-    overflow-x: hidden; 
-	font-family: 'Arimo', sans-serif;
-}
+$(document).ready(function() {
+	var pesos = true;
+var exchange = 620;
+	$("#sant").show();
+	$("#vina").hide();
+	$("#ski").hide();
+	$("#playa").hide();
+	$("#ent").hide();
+	$("#nos").hide();
+	$("#molde").hide();
 
-nav .container-fluid{
-	background-color: rgba(58, 68, 122, 0.50);
-	font-weight: bold;
-}
+	$("#s1").click(function() {
+		$("#vina").hide();
+		$("#ski").hide();
+		$("#playa").hide();
+		$("#ent").hide();
+		$("#nos").hide();
+		$("#sant").show();
+	});
 
-#page{
-	background: #CC4738;
-}
-.bkgnd{
-	margin: 5px;
-	background: #eee;
-}
+	$("#s2").click(function() {
+		$("#sant").hide();
+		$("#ski").hide();
+		$("#playa").hide();
+		$("#ent").hide();
+		$("#nos").hide();
+		$("#vina").show();
+	});
 
-#first{
-	padding-top: 50px;
-	font-weight: bold;
-	font-size: 25px;
-}
+	$("#s3").click(function() {
+		$("#sant").hide();
+		$("#vina").hide();
+		$("#playa").hide();
+		$("#ent").hide();
+		$("#nos").hide();
+		$("#ski").show();
+	});
 
-#header{
-	background-image: url(https://upload.wikimedia.org/wikipedia/commons/3/3e/Santiago_de_Chile_de_noche.jpg);
-  background-position: center center;
-  background-repeat: no-repeat;  
-  background-attachment: fixed;
-  background-size: cover;
-  background-color: #562f2f;
-	position: relative;
-	height: 550px;
-}
+	$("#s4").click(function() {
+		$("#sant").hide();
+		$("#vina").hide();
+		$("#ski").hide();
+		$("#nos").hide();
+		$("#ent").hide();
+		$("#playa").show();
+	});
 
-#logo{
-	display: block;
-	position: absolute;
-  margin-left: auto;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-	width: 50%;
-}
+	$("#s5").click(function() {
+		$("#sant").hide();
+		$("#vina").hide();
+		$("#ski").hide();
+		$("#playa").hide();
+		$("#nos").hide();
+		$("#ent").show();
+	});
 
-.navbar-inverse{
-	background-color:rgba(0,0,0,0.85);
-	z-index: 9999;
-}
+	$("#s6").click(function() {
+		$("#sant").hide();
+		$("#vina").hide();
+		$("#playa").hide();
+		$("#ski").hide();
+		$("#ent").hide();
+		$("#nos").show();
+	});
 
-.section{
-	padding: 2% 5% 2% 5%;
+	$(".vmaipo").click(function() {
+		$("#vmaipo").slideToggle();
+		var $this = $(".toggm");
+		var toggled = $this.hasClass("fa-chevron-circle-down");
+		$this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
+	});
+
+	$(".vcasa").click(function() {
+		$("#vcasa").slideToggle();
+		var $this = $(".toggc");
+		var toggled = $this.hasClass("fa-chevron-circle-down");
+		$this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
+	});
+
+	$(".vcolcha").click(function() {
+		$("#vcolcha").slideToggle();
+		var $this = $(".toggco");
+		var toggled = $this.hasClass("fa-caret-square-o-down");
+		$this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
+	});
+
+	$("#pre").click(function() {
+		$("#preguntas").slideToggle();
+	});
 	
-}
-
-.section h1{
-	text-align: center;
-	font-size: 3em;
-	font-family: 'Acme', sans-serif;
-}
-
-.title{
-	font-weight: bold;
-	text-align: center;
-	padding-bottom: 1%;
-	padding-top: 2%;
-	font-family: 'Fjalla One', sans-serif;
-}
-
-hr{
-background-color: #aaa;
-height: 1px;
-}
-
-.-color: black;itemtext{
-	border: 2px #ccc solid;
-	border-radius: 20px;
-	padding: 2%;
-	text-align: justify;
-	display: inline-block;
-	font-size: 1.2em;
-}
-
-
-	.carousel-inner > .item > img, .carousel-inner > .item > a > img {
-    width: 100%;
-}
-
-.itemtext{
-	border: 1px solid #ccc;
-    border-radius: 10px;
-	text-align: justify;
-	padding: 10px 20px;
-	font-size: 105%;
-	background-color: rgb(255,255,255);
-}
-
-.subtit{
-	font-size: 150%;
-	font-family: 'Fjalla One', sans-serif;
-}
-
-.descripcion{
-	text-indent: 20px;
-	margin-top:5%;
-}
-
-.precios{
-	background-color: #5C6BC0;
-	font-weight: bold;
-	color: white;
-	width: 26%;
-	height:8%;
-	font-size: 3.4vw;
-	text-align: center;
-	vertical-align: middle;
-	padding: 0;
-	display:table-cell;
-	position: absolute;
-	bottom: 0;
-	right: 0;
-	border-radius: 0;
-	border: 2px solid;
-	z-index: 1;
 	
-	-webkit-user-select: none;  
-  -moz-user-select: none;    
-  -ms-user-select: none;      
-  user-select: none;
-	cursor: pointer;
-}
-
-.extra{
-	position: relative;
-
-}
-
-.incluye{
-	width:100%;
-}
-
-.opcional{
-	width:100%;
-}
-
-
-.vmaipo:hover{
-	color: black;
-	cursor: pointer;
-}
-
-.vcasa:hover{
-	color: black;
-	cursor: pointer;
-}
-
-.vcolcha:hover{
-	color: black;
-	cursor: pointer;
-}
-
-#vmaipo{
-	display: none;
-}
-
-#vcasa{
-	display: none;
-}
-
-#vcolcha{
-	display:none;
-}
-
-#pre:hover{
-	color:black;
-	cursor:pointer;
-}
-#preguntas{
-	display:none;
-}
-
-.imgdim{	
-	position: relative;
-  background-repeat: no-repeat;
-  background-size: cover;
-	
-}
-
-#myCarousel{
-	width: 100%;
-	margin-left: auto;
-	margin-right: auto;
-	margin-bottom: 4%;
-}
-
-#santa{
-	position: relative;
-	width: 50%;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-#footer{
-	background-color: #5C6BC0;
-	padding: 20px 20px 20px 20px;
-	text-align: center;
-}
-
-#footer .itemtext{
-	background-color: rgba(255,255,255,0.75);
-}
-
-#footer h2{
-	font-family: 'Fjalla One', sans-serif;
-}
-
-.icon{
-	font-size: 250%;
-	color: #CC4738;
-}
-
-.red{
-	color: #404a86;
-	padding-bottom: 15px;
-	padding-top: 10px;
-}
-
-.red:hover{
-	color: #9da6d9;
-}
-
-.pay{
-	text-align: center;
-	
-}
-
-.pay img{
-	max-width: 100%;
-    height: auto;
-}
-
-#lang{
-	position: relative;
-	left: 45%
-}
-
-#dev a{
-	color: #ff6117;
-	font-weight: bold;
-}
-
-#dev a:hover{
-	color: #993a0d;
-}
+	$(".precios").click(function() {
+		$('#mess').hide();
+		if (pesos == true) {
+			precio = $(".precios").text().replace(/[^0-9]/gi, '');
+			precio /= exchange;
+			resultado = Math.round(precio);
+			$(".precios").text(resultado + " USD");
+			pesos = false;
+		} else {
+			precio *= exchange;
+			resultado = Math.round(precio);
+			$(".precios").text(resultado.toLocaleString() + " CLP");
+			pesos = true;
+		}
+	});
+});
