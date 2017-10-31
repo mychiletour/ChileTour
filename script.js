5c58b12b70ef7209ecc1431cfd62ab63d46c47dd
Kevin Castillo <kevinacastles@gmail.com>
2017-10-31 18:19:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	
+	var pesos = true;
+var exchange = 620;
 	$("#sant").show();
 	$("#vina").hide();
 	$("#ski").hide();
@@ -9,84 +10,97 @@ $(document).ready(function() {
 	$("#molde").hide();
 
 	$("#s1").click(function() {
-	$("#vina").hide();
-	$("#ski").hide();
-	$("#playa").hide();
-	$("#ent").hide();
-	$("#nos").hide();
+		$("#vina").hide();
+		$("#ski").hide();
+		$("#playa").hide();
+		$("#ent").hide();
+		$("#nos").hide();
 		$("#sant").show();
 	});
-	
+
 	$("#s2").click(function() {
-	$("#sant").hide();
-	$("#ski").hide();
+		$("#sant").hide();
+		$("#ski").hide();
 		$("#playa").hide();
-	$("#ent").hide();
-	$("#nos").hide();
+		$("#ent").hide();
+		$("#nos").hide();
 		$("#vina").show();
 	});
-	
-		$("#s3").click(function() {
-			$("#sant").hide();
-	$("#vina").hide();
-			$("#playa").hide();
-	$("#ent").hide();
-	$("#nos").hide();
+
+	$("#s3").click(function() {
+		$("#sant").hide();
+		$("#vina").hide();
+		$("#playa").hide();
+		$("#ent").hide();
+		$("#nos").hide();
 		$("#ski").show();
 	});
-	
+
 	$("#s4").click(function() {
-	$("#sant").hide();
-	$("#vina").hide();
-	$("#ski").hide();
-	$("#nos").hide();
+		$("#sant").hide();
+		$("#vina").hide();
+		$("#ski").hide();
+		$("#nos").hide();
 		$("#ent").hide();
 		$("#playa").show();
-		
 	});
-	
-		$("#s5").click(function() {
-	$("#sant").hide();
-	$("#vina").hide();
-	$("#ski").hide();
-			$("#playa").hide();
-	$("#nos").hide();
+
+	$("#s5").click(function() {
+		$("#sant").hide();
+		$("#vina").hide();
+		$("#ski").hide();
+		$("#playa").hide();
+		$("#nos").hide();
 		$("#ent").show();
 	});
-	
-		$("#s6").click(function() {
-	$("#sant").hide();
-	$("#vina").hide();
-			$("#playa").hide();
-	$("#ski").hide();
-	$("#ent").hide();
+
+	$("#s6").click(function() {
+		$("#sant").hide();
+		$("#vina").hide();
+		$("#playa").hide();
+		$("#ski").hide();
+		$("#ent").hide();
 		$("#nos").show();
 	});
-	
-	$(".vmaipo").click(function(){
-            $("#vmaipo").slideToggle();
-    var $this = $('.toggm');
-    var toggled = $this.hasClass("fa-chevron-circle-down");
-    $this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
-});
-	
-	$(".vcasa").click(function(){
-            $("#vcasa").slideToggle();
-		var $this = $('.toggc');
-    var toggled = $this.hasClass("fa-chevron-circle-down");
-    $this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
-        });
-	
-	$(".vcolcha").click(function(){
-            $("#vcolcha").slideToggle();
-		var $this = $('.toggco');
-    var toggled = $this.hasClass("fa-caret-square-o-down");
-    $this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
-        });
-	
-	$("#pre").click(function(){
-            $("#preguntas").slideToggle();
-        });
-	
-	
+
+	$(".vmaipo").click(function() {
+		$("#vmaipo").slideToggle();
+		var $this = $(".toggm");
+		var toggled = $this.hasClass("fa-chevron-circle-down");
+		$this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
+	});
+
+	$(".vcasa").click(function() {
+		$("#vcasa").slideToggle();
+		var $this = $(".toggc");
+		var toggled = $this.hasClass("fa-chevron-circle-down");
+		$this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
+	});
+
+	$(".vcolcha").click(function() {
+		$("#vcolcha").slideToggle();
+		var $this = $(".toggco");
+		var toggled = $this.hasClass("fa-caret-square-o-down");
+		$this.toggleClass("fa-chevron-circle-up fa-chevron-circle-down");
+	});
+
+	$("#pre").click(function() {
+		$("#preguntas").slideToggle();
+	});
+	var clp;
+	var dollar;
+	$(".precios").click(function() {
+		if (pesos == true) {
+			precio = $(".precios").text();
+			precio /= exchange;
+			resultado = Math.round(precio);
+			$(".precios").text(resultado);
+			pesos = false;
+		} else {
+			precio *= exchange;
+			resultado = Math.round(precio);
+			$(".precios").text(resultado);
+			pesos = true;
+		}
+	});
 });
