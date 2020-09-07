8832e023a9cef2d0bb1b0a3bfc01f732c3bf68a5
KevCastles <kevinacastles@gmail.com>
2020-09-07 17:10:05 -0300

    change routes for domain name

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -13,31 +13,31 @@ const Navibar = () => {
                 <Nav className="mr-auto">
                     <NavDropdown className="mr-5" title="Santiago" id="collasible-nav-dropdown">
                         <NavLink className="navlink-item" exact to="/ChileTour">Encargos y Traslados</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/SantiagoTours">Tours por Santiago</NavLink>
+                        <NavLink className="navlink-item" to="/SantiagoTours">Tours por Santiago</NavLink>
                     </NavDropdown>
                     <NavDropdown className="mr-5" title="Viñedos" id="collasible-nav-dropdown">
-                        <NavLink className="navlink-item" to="/ChileTour/ValleMaipo">Valle del Maipo</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/ValleCasablanca">Valle de Casablanca</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/ValleColchagua">Valle de Colchagua</NavLink>
+                        <NavLink className="navlink-item" to="/ValleMaipo">Valle del Maipo</NavLink>
+                        <NavLink className="navlink-item" to="/ValleCasablanca">Valle de Casablanca</NavLink>
+                        <NavLink className="navlink-item" to="/ValleColchagua">Valle de Colchagua</NavLink>
                     </NavDropdown>
                     <NavDropdown className="mr-5" title="Centros de Ski" id="collasible-nav-dropdown">
-                        <NavLink className="navlink-item" to="/ChileTour/ValleNevado">Valle Nevado</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/Farellones">Farellones</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/ElColorado">El Colorado</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/LaParva">La Parva</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/Portillo">Portillo</NavLink>
+                        <NavLink className="navlink-item" to="/ValleNevado">Valle Nevado</NavLink>
+                        <NavLink className="navlink-item" to="/Farellones">Farellones</NavLink>
+                        <NavLink className="navlink-item" to="/ElColorado">El Colorado</NavLink>
+                        <NavLink className="navlink-item" to="/LaParva">La Parva</NavLink>
+                        <NavLink className="navlink-item" to="/Portillo">Portillo</NavLink>
                     </NavDropdown>
                     <NavDropdown className="mr-5" title="Playas y Cruceros" id="collasible-nav-dropdown">
-                        <NavLink className="navlink-item" to="/ChileTour/Valparaiso">Viña del Mar y Valparaíso</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/IslaNegra">Isla Negra</NavLink>
-                        <NavLink className="navlink-item" to="/ChileTour/Cruceros">Cruceros</NavLink>
+                        <NavLink className="navlink-item" to="/Valparaiso">Viña del Mar y Valparaíso</NavLink>
+                        <NavLink className="navlink-item" to="/IslaNegra">Isla Negra</NavLink>
+                        <NavLink className="navlink-item" to="/Cruceros">Cruceros</NavLink>
                     </NavDropdown>
                     <NavDropdown className="mr-5" title="Entretenimientos" id="collasible-nav-dropdown">
-                    <NavLink className="navlink-item" to="/ChileTour/Monticello">Casino Sun Monticello</NavLink>
-                    <NavLink className="navlink-item" to="/ChileTour/EnjoySantiago">Casino Enjoy Santiago</NavLink>
-                    <NavLink className="navlink-item" to="/ChileTour/BaliHai">Cena Show Bali Hai</NavLink>
-                    <NavLink className="navlink-item" to="/ChileTour/AutosAntiguos">Museo de Autos Antiguos</NavLink>
-                    <NavLink className="navlink-item" to="/ChileTour/Safari">Parque Safari</NavLink>
+                    <NavLink className="navlink-item" to="/Monticello">Casino Sun Monticello</NavLink>
+                    <NavLink className="navlink-item" to="/EnjoySantiago">Casino Enjoy Santiago</NavLink>
+                    <NavLink className="navlink-item" to="/BaliHai">Cena Show Bali Hai</NavLink>
+                    <NavLink className="navlink-item" to="/AutosAntiguos">Museo de Autos Antiguos</NavLink>
+                    <NavLink className="navlink-item" to="/Safari">Parque Safari</NavLink>
                     </NavDropdown>
 
                 </Nav>
